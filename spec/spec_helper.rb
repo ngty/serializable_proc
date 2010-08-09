4c4f852ecea0327e7c0f5900500e7317ea924103
NgTzeYang <ngty77@gmail.com>
2010-08-09 15:43:05 +0800

    Initial commit to serializable_proc.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,8 @@
+require 'rubygems'
+require 'bacon'
+
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'serializable_proc'
+
+Bacon.summary_on_exit
