0c96c0c62c9f2b60b7002926a8c535bc04e34752
NgTzeYang <ngty77@gmail.com>
2010-08-09 19:33:10 +0800

    Improved modularity.

--- lib/serializable_proc.rb
+++ lib/serializable_proc.rb
@@ -1,25 +1,29 @@
 require 'rubygems'
-require 'ruby_parser'
+require 'forwardable'
 require 'ruby2ruby'
+require 'ruby_parser'
 
 class SerializableProc
 
   RUBY_PARSER = RubyParser.new
   RUBY_2_RUBY = Ruby2Ruby.new
 
-  attr_reader :file, :line, :code
+  extend Forwardable
+  %w{line file arity}.each{|meth| def_delegator :@proc, meth.to_sym }
+
+  attr_reader :code
 
   def initialize(&block)
-    @block = block
-    extract_file_and_line_and_code
+    @proc = Proc.new(block)
+    @code = extract_code(Patterns.new(@proc))
   end
 
   def marshal_dump
-    [@code, @file, @line]
+    [@code, @proc]
   end
 
   def marshal_load(data)
-    @code, @file, @line = data
+    @code, @proc = data
   end
 
   private
@@ -28,15 +32,16 @@ class SerializableProc
       eval(@code, nil, @file, @line)
     end
 
-    def extract_file_and_line_and_code
-      code, remaining = code_fragments
+    def extract_code(patterns)
+      ignore, start_marker, arg = [:ignore, :start_marker, :arg].map{|key| patterns.match_args[key] }
+      remaining = @proc.raw_code.sub(ignore, '')
+      code = arg ? "proc #{start_marker} |#{arg}|" : "proc #{start_marker}"
 
-      while frag = remaining[frag_regexp,1]
+      while frag = remaining[patterns[:frag],1]
         begin
-          sexp = RUBY_PARSER.parse(replace_magic_vars(code += frag))
-          if sexp.inspect =~ sexp_regexp
-            @code = revert_magic_vars(RUBY_2_RUBY.process(sexp)).sub('proc {','lambda {')
-            break
+          sexp = RUBY_PARSER.parse(escape_magic_vars(code += frag))
+          if sexp.inspect =~ patterns[:sexp]
+            return unescape_magic_vars(RUBY_2_RUBY.process(sexp)).sub('proc {','lambda {')
           end
         rescue SyntaxError, Racc::ParseError, NoMethodError
           remaining.sub!(frag,'')
@@ -44,72 +49,81 @@ class SerializableProc
       end
     end
 
-    def code_fragments
-      ignore, start_marker, arg =
-        [:ignore, :start_marker, :arg].map{|key| code_match_args[key] }
-      [
-        arg ? "proc #{start_marker} |#{arg}|" : "proc #{start_marker}",
-        source_code.sub(ignore, '')
-      ]
-    end
-
-    def sexp_regexp
-      @sexp_regexp ||= (
-        Regexp.new([
-          Regexp.quote("s(:iter, s(:call, nil, :"),
-          "(proc|lambda)",
-          Regexp.quote(", s(:arglist)), "),
-          '(%s|%s|%s)' % [
-            Regexp.quote('s(:masgn, s(:array, s('),
-            Regexp.quote('s(:lasgn, :'),
-            Regexp.quote('nil, s(')
-          ]
-        ].join)
-      )
-    end
-
-    def frag_regexp
-      @frag_regexp ||= (
-        end_marker = {'do' => 'end', '{' => '\}'}[code_match_args[:start_marker]]
-        /^(.*?\W#{end_marker})/m
-      )
-    end
-
-    def code_regexp
-      @code_regexp ||=
-        /^(.*?(SerializableProc\.new|lambda|proc|Proc\.new)?\s*(do|\{)\s*(\|(.*?)\|\s*)?)/m
-    end
-
-    def code_match_args
-      @code_match_args ||= (
-        args = source_code.match(code_regexp)
-        {
-          :ignore => args[1],
-          :start_marker => args[3],
-          :arg => args[5]
-        }
-      )
-    end
-
-    def source_code
-      @source_code ||= (
-        file, line = /^#<Proc:0x[0-9A-Fa-f]+@(.+):(\d+).*?>$/.match(@block.inspect)[1..2]
-        @file = File.expand_path(file)
-        @line = line.to_i
-        File.readlines(@file)[@line.pred .. -1].join
-      )
-    end
-
-    def replace_magic_vars(code)
+    def escape_magic_vars(code)
       %w{__FILE__ __LINE__}.inject(code) do |code, var|
         code.gsub(var, "%|((#{var}))|")
       end
     end
 
-    def revert_magic_vars(code)
+    def unescape_magic_vars(code)
       %w{__FILE__ __LINE__}.inject(code) do |code, var|
         code.gsub(%|"((#{var}))"|, var)
       end
+    end
+
+    class Proc
+
+      attr_reader :file, :line, :arity, :raw_code
+
+      def initialize(block)
+        file, line = /^#<Proc:0x[0-9A-Fa-f]+@(.+):(\d+).*?>$/.match(block.inspect)[1..2]
+        @file, @line, @arity = File.expand_path(file), line.to_i, block.arity
+        @raw_code = File.readlines(@file)[@line.pred .. -1].join
+      end
+
+    end
+
+    class Patterns
+
+      def initialize(context)
+        @context = context
+      end
+
+      def match_args
+        @match_args ||= (
+          args = @context.raw_code.match(code_regexp)
+          {
+            :ignore => args[1],
+            :start_marker => args[3],
+            :arg => args[5]
+          }
+        )
+      end
+
+      def [](key)
+        (
+          @cache ||= {
+            :sexp => sexp_regexp,
+            :code => code_regexp,
+            :frag => frag_regexp,
+          }
+        )[key]
+      end
+
+      private
+
+        def sexp_regexp
+          Regexp.new([
+            Regexp.quote("s(:iter, s(:call, nil, :"),
+            "(proc|lambda)",
+            Regexp.quote(", s(:arglist)), "),
+            '(%s|%s|%s)' % [
+              Regexp.quote('s(:masgn, s(:array, s('),
+              Regexp.quote('s(:lasgn, :'),
+              Regexp.quote('nil, s(')
+            ]
+          ].join)
+        end
+
+        def frag_regexp
+          end_marker = {'do' => 'end', '{' => '\}'}[match_args[:start_marker]]
+          /^(.*?\W#{end_marker})/m
+        end
+
+        def code_regexp
+          /^(.*?(SerializableProc\.new|lambda|proc|Proc\.new)?\s*(do|\{)\s*(\|(.*?)\|\s*)?)/m
+        end
+
     end
 
 end
