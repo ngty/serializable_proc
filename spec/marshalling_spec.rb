d0e9841b54a7ae2b312521fdd5dab0a5c0217746
NgTzeYang <ngty77@gmail.com>
2010-08-11 00:38:20 +0800

    Yup, behavious after marshalling work as expected.

--- spec/marshalling_spec.rb
+++ spec/marshalling_spec.rb
@@ -2,17 +2,49 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 
 describe 'Marshalling' do
 
-  before do
-    @proc = SerializableProc.new{ %w{a b}.map{|x| x } }
+  describe '>> basic (wo contextual references)' do
+
+    before do
+      @proc = SerializableProc.new{ %w{a b}.map{|x| x } }
+    end
+
+    should 'be able to marshal' do
+      Marshal.load(Marshal.dump(@proc))
+      true.should.be.true # the above should execute wo error
+    end
+
+    should 'be able to resume proc behaviours after marshal' do
+      Marshal.load(Marshal.dump(@proc)).call.should.equal(%w{a b})
+    end
+
   end
 
-  should 'be able to marshal' do
-    Marshal.load(Marshal.dump(@proc))
-    true.should.be.true # the above should execute wo error
-  end
+  describe '>> with contextual references' do
 
-  should 'be able to resume proc behaviours after marshal' do
-    Marshal.load(Marshal.dump(@proc)).call.should.equal(%w{a b})
+    should 'handle local variables' do
+      x, y, expected = 'awe', 'some', 'awesome'
+      s_proc = SerializableProc.new{ x + y }
+      Marshal.load(Marshal.dump(s_proc)).call.should.equal(expected)
+    end
+
+    should 'handle instance variables' do
+      @x, @y, expected = 'awe', 'some', 'awesome'
+      s_proc = SerializableProc.new{ @x + @y }
+      Marshal.load(Marshal.dump(s_proc)).call.should.equal(expected)
+    end
+
+    should 'handle class variables' do
+      @@x, @@y, expected = 'awe', 'some', 'awesome'
+      s_proc = SerializableProc.new{ @@x + @@y }
+      Marshal.load(Marshal.dump(s_proc)).call.should.equal(expected)
+    end
+
+    should 'handle global variables' do
+      $x, $y, expected = 'awe', 'some', 'awesome'
+      s_proc = SerializableProc.new{ $x + $y }
+      Marshal.load(Marshal.dump(s_proc)).call.should.equal(expected)
+    end
+
   end
 
 end
