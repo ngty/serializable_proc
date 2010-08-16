6e6969a72942b50383d72e639707b23d2f9b2e7a
NgTzeYang <ngty77@gmail.com>
2010-08-16 22:56:53 +0800

    It is now possible to optionally turn off/on isolating of all variables.

--- lib/serializable_proc/binding.rb
+++ lib/serializable_proc/binding.rb
@@ -8,19 +8,18 @@ class SerializableProc
     marshal_attr :vars
 
     def initialize(binding, sexp)
-      @vars, flag = {}, '@@_isolate_globals'
-      sexp_str = sexp.gsub(s(:scope, s(:block, SexpAny.new)), nil).inspect
-      types = sexp_str.include?("s(:cvdecl, :#{flag}, s(:true))") ? %w{l g c i} : %w{l c i}
-
-      while m = sexp_str.match(/^(.*?s\(:(?:#{types.join('|')})var, :([^\)]+)\))/)
-        ignore, var = m[1..2]
-        sexp_str.sub!(ignore,'')
-        next if var == flag
-        begin
-          val = eval(var, binding) rescue nil
-          @vars.update(Sandboxer.fvar(var) => mclone(val))
-        rescue TypeError
-          raise CannotSerializeVariableError.new("Variable #{var} cannot be serialized !!")
+      @sexp, @vars = sexp.gsub(s(:scope, s(:block, SexpAny.new)), nil), {}
+      unless (types = extract_isolated_types).empty?
+        sexp_str = @sexp.inspect
+        while m = sexp_str.match(/^(.*?s\(:(?:#{types.join('|')})var, :([^\)]+)\))/)
+          ignore, var = m[1..2]
+          sexp_str.sub!(ignore,'')
+          begin
+            val = eval(var, binding) rescue nil
+            @vars.update(Sandboxer.fvar(var) => mclone(val))
+          rescue TypeError
+            raise CannotSerializeVariableError.new("Variable #{var} cannot be serialized !!")
+          end
         end
       end
     end
@@ -34,25 +33,40 @@ class SerializableProc
       )
     end
 
-    module Extensions
-      def self.extended(base)
-        class << base
+    private
 
-          alias_method :orig_eval, :eval
+      def extract_isolated_types
+        o_sexp_arry = @sexp.to_a
+        @sexp = @sexp.gsub(s(:cvdecl, :@@_isolate_vars, SexpAny.new), nil)
+        types = %w{global instance local class}
 
-          def eval(str)
-            begin
-              @fvar = Sandboxer.fvar(str).to_s
-              orig_eval(@fvar)
-            rescue NameError => e
-              msg = e.message.sub(@fvar, str)
-              raise NameError.new(msg)
-            end
-          end
-
+        if (diff = o_sexp_arry - @sexp.to_a).empty?
+          types.map{|t| t[0].chr }
+        else
+          sexp_str = Sexp.from_array(diff).inspect
+          types.map{|t| t[0].chr if sexp_str.include?("s(:lit, :#{t})") }.compact
         end
       end
-    end
+
+      module Extensions
+        def self.extended(base)
+          class << base
+
+            alias_method :orig_eval, :eval
+
+            def eval(str)
+              begin
+                @fvar = Sandboxer.fvar(str).to_s
+                orig_eval(@fvar)
+              rescue NameError => e
+                msg = e.message.sub(@fvar, str)
+                raise NameError.new(msg)
+              end
+            end
+
+          end
+        end
+      end
 
   end
 end
