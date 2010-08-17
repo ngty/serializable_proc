929efe8248fdcab220a265e9fcfae915be7f82b5
NgTzeYang <ngty77@gmail.com>
2010-08-17 15:05:52 +0800

    We don want this to appear in the generated rdoc.

--- lib/serializable_proc/fixes.rb
+++ lib/serializable_proc/fixes.rb
@@ -1,7 +1,7 @@
 # NOTE: This file contains hackeries to ensure compatibility with different rubies
 
 unless 1.respond_to?(:pred)
-  class Integer
+  class Integer #:nodoc:
     def pred
       self - 1
     end
