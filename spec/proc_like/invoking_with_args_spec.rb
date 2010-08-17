d2d07d41abaed57d09129a524ef5b5710c007c68
NgTzeYang <ngty77@gmail.com>
2010-08-17 14:16:10 +0800

    Bounded variables specs are relocated to the broken down specs, this spec now focuses on args passing.

---
+++ spec/proc_like/invoking_with_args_spec.rb
@@ -0,0 +1,58 @@
+require File.join(File.dirname(__FILE__), '..', 'spec_helper')
+
+[:[], :call].each do |invoke|
+  describe "Invoking with args using :#{invoke}" do
+
+    describe '>> wo specified binding' do
+
+      should 'return yield result given no arg' do
+        s_proc = SerializableProc.new { %w{b c}.map{|x| x } }
+        s_proc.send(invoke).should.equal(%w{b c})
+      end
+
+      should 'return yield result given single arg' do
+        s_proc = SerializableProc.new {|i| %w{b c}.map{|x| x*i } }
+        s_proc.send(invoke, 2).should.equal(%w{bb cc})
+      end
+
+      should 'return yield result given optional args' do
+        s_proc = SerializableProc.new {|*args| %w{b c}.map{|x| [x,args].flatten.join } }
+        s_proc.send(invoke, '1', '2').should.equal(%w{b12 c12})
+      end
+
+      should 'return yield result given multiple args' do
+        s_proc = SerializableProc.new {|i, j| %w{b c}.map{|x| x*i*j } }
+        s_proc.send(invoke, 2, 3).should.equal(%w{bbbbbb cccccc})
+      end
+
+    end
+
+    describe '>> w specified binding' do
+
+      should 'return yield result given no arg' do
+        x = 'a'
+        s_proc = SerializableProc.new { %w{b c}.map{|x| x } }
+        s_proc.send(invoke, binding).should.equal(%w{b c})
+      end
+
+      should 'return yield result given single arg' do
+        x = 'a'
+        s_proc = SerializableProc.new {|i| %w{b c}.map{|x| x*i } }
+        s_proc.send(invoke, 2, binding).should.equal(%w{bb cc})
+      end
+
+      should 'return yield result given optional args' do
+        x = 'a'
+        s_proc = SerializableProc.new {|*args| %w{b c}.map{|x| [x,args].flatten.join } }
+        s_proc.send(invoke, '1', '2', binding).should.equal(%w{b12 c12})
+      end
+
+      should 'return yield result given multiple args' do
+        x = 'a'
+        s_proc = SerializableProc.new {|i, j| %w{b c}.map{|x| x*i*j } }
+        s_proc.send(invoke, 2, 3, binding).should.equal(%w{bbbbbb cccccc})
+      end
+
+    end
+  end
+end
