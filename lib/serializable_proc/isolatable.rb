5a5396fd906894506f0fd3c071603025a7ebd65c
NgTzeYang <ngty77@gmail.com>
2010-08-18 21:53:36 +0800

    Killed the monsterous code, cleaned-up SerializableProc::Isolatable#isolated_sexp.

--- lib/serializable_proc/isolatable.rb
+++ lib/serializable_proc/isolatable.rb
@@ -4,32 +4,39 @@ class SerializableProc
     protected
 
       def isolated_sexp(sexp)
-        # TODO: This nasty mess needs some loving touch !!
-        block_replace = :_serializable_proc_block_scope_marker_
-        t_sexp = sexp.gsub(s(:scope, s(:block, SexpAny.new)), block_replace)
-        blocks_pattern = %r{^#{Regexp.quote(t_sexp.inspect).gsub(block_replace.inspect,'(.*?)')}$}
-        blocks = sexp.inspect.match(blocks_pattern)[1..-1] rescue []
-        n_sexp_str = nil
-
-        unless (types = isolated_types(sexp)).empty?
-          var_pattern = /^(.*?s\(:)((#{types.join('|')})(asgn|var|vdecl))(,\ :)((|@|@@|\$)([\w]+))(\)|,)/
-          t_sexp_str = t_sexp.inspect
-
-          while m = t_sexp_str.match(var_pattern)
-            orig, prepend, _, type, declare, join, var, _, name, append = m[0..-1]
-            n_sexp_str = isolatable?(var) ?
-              "#{n_sexp_str}#{prepend}l#{declare.sub('vdecl','asgn')}#{join}#{type}var_#{name}#{append}" :
-              "#{n_sexp_str}#{orig}"
-            t_sexp_str.sub!(orig,'')
+        eval (types = isolated_types(sexp)).empty? ? sexp.inspect : (
+          bypass_scoping_by_block(sexp) do |sexp_str|
+            # NOTE: for performance issue, we play around with the sexp string, rather
+            # than the actual sexp.
+            isolated_sexp_str_for_typed_vars(sexp_str, types)
           end
+        )
+      end
 
-          n_sexp_str = n_sexp_str.nil? ? nil :
-            blocks.inject("#{n_sexp_str}#{t_sexp_str}") do |sexp_str, block_str|
-              sexp_str.sub(block_replace.inspect, block_str)
-            end
+      def bypass_scoping_by_block(sexp)
+        tmp_marker = :_serializable_proc_block_scope_marker_
+        s_sexp = sexp.gsub(s(:scope, s(:block, SexpAny.new)), tmp_marker)
+        pattern = %r{^#{Regexp.quote(s_sexp.inspect).gsub(tmp_marker.inspect,'(.*?)')}$}
+        orig_blocks = sexp.inspect.match(pattern)[1..-1] rescue []
+        n_sexp_str = yield(s_sexp.inspect)
+        orig_blocks.inject(n_sexp_str) do |sexp_str, block_sexp_str|
+          sexp_str.sub(tmp_marker.inspect, block_sexp_str)
+        end
+      end
+
+      def isolated_sexp_str_for_typed_vars(o_sexp_str, types)
+        n_sexp_str = nil
+        var_pattern = /^(.*?s\(:)((#{types.join('|')})(asgn|var|vdecl))(,\ :)((|@|@@|\$)([\w]+))(\)|,)/
+
+        while m = o_sexp_str.match(var_pattern)
+          orig, prepend, _, type, declare, join, var, _, name, append = m[0..-1]
+          n_sexp_str = isolatable?(var) ?
+            "#{n_sexp_str}#{prepend}l#{declare.sub('vdecl','asgn')}#{join}#{type}var_#{name}#{append}" :
+            "#{n_sexp_str}#{orig}"
+          o_sexp_str.sub!(orig,'')
         end
 
-        eval(n_sexp_str ? n_sexp_str : sexp.inspect)
+        "#{n_sexp_str}#{o_sexp_str}"
       end
 
       def isolated_var(var)
