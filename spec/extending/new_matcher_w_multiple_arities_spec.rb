382fd4ccc94ba5244bdc70587b784b04f67c928f
NgTzeYang <ngty77@gmail.com>
2010-08-18 10:11:03 +0800

    Preparation for adding more complex matcher support.

--- spec/extending/new_matcher_w_multiple_arities_spec.rb
+++ spec/extending/new_matcher_w_multiple_arities_spec.rb
@@ -2,75 +2,79 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 
 describe 'New matcher w multiple arities' do
 
-  extend SerializableProc::Spec::Helpers
   expected_file = File.expand_path(__FILE__)
   expected_code = "lambda { |lvar_arg1, lvar_arg2| [\"a\", \"b\"].map { |lvar_x| puts(lvar_x) } }"
 
-  behaves_like 'has support for parsing Otaky.work (wo args)'
+  describe '>> wo args' do
 
-  should "handle block using do ... end [##{__LINE__}]" do
-    (
-      Otaky.work do |arg1, arg2|
-        %w{a b}.map{|x| puts x }
-      end
-    ).should.be having_expected_proc_attrs(expected_file, __LINE__ - 3, expected_code)
-  end
+    extend SerializableProc::Spec::Helpers
+    behaves_like 'has support for parsing Otaky.work (wo args)'
 
-  should "handle block using do ... end [##{__LINE__}]" do
-    (Otaky.work do |arg1, arg2| %w{a b}.map{|x| puts x } end).
-      should.be having_expected_proc_attrs(expected_file, __LINE__.pred, expected_code)
-  end
+    should "handle block using do ... end [##{__LINE__}]" do
+      (
+        Otaky.work do |arg1, arg2|
+          %w{a b}.map{|x| puts x }
+        end
+      ).should.be having_expected_proc_attrs(expected_file, __LINE__ - 3, expected_code)
+    end
 
-  should "handle block using { ... } [##{__LINE__}]" do
-    (
-      Otaky.work { |arg1, arg2|
-        %w{a b}.map{|x| puts x }
-      }
-    ).should.be having_expected_proc_attrs(expected_file, __LINE__ - 3, expected_code)
-  end
+    should "handle block using do ... end [##{__LINE__}]" do
+      (Otaky.work do |arg1, arg2| %w{a b}.map{|x| puts x } end).
+        should.be having_expected_proc_attrs(expected_file, __LINE__.pred, expected_code)
+    end
 
-  should "handle block using { ... } [##{__LINE__}]" do
-    (Otaky.work { |arg1, arg2| %w{a b}.map{|x| puts x } }).
-      should.be having_expected_proc_attrs(expected_file, __LINE__.pred, expected_code)
-  end
+    should "handle block using { ... } [##{__LINE__}]" do
+      (
+        Otaky.work { |arg1, arg2|
+          %w{a b}.map{|x| puts x }
+        }
+      ).should.be having_expected_proc_attrs(expected_file, __LINE__ - 3, expected_code)
+    end
 
-  should "handle fanciful initializing with lambda { ... } [##{__LINE__}]" do
-    (Otaky.work(&(lambda { |arg1, arg2| %w{a b}.map{|x| puts x } }))).
-      should.be having_expected_proc_attrs(expected_file, __LINE__.pred, expected_code)
-  end
+    should "handle block using { ... } [##{__LINE__}]" do
+      (Otaky.work { |arg1, arg2| %w{a b}.map{|x| puts x } }).
+        should.be having_expected_proc_attrs(expected_file, __LINE__.pred, expected_code)
+    end
 
-  should "handle fanciful initializing with lambda do ... end [##{__LINE__}]" do
-    (
-      Otaky.work(&(lambda do |arg1, arg2|
-        %w{a b}.map{|x| puts x }
-      end))
-    ).should.be having_expected_proc_attrs(expected_file, __LINE__ - 3, expected_code)
-  end
+    should "handle fanciful initializing with lambda { ... } [##{__LINE__}]" do
+      (Otaky.work(&(lambda { |arg1, arg2| %w{a b}.map{|x| puts x } }))).
+        should.be having_expected_proc_attrs(expected_file, __LINE__.pred, expected_code)
+    end
 
-  should "handle fanciful initializing with proc { ... } [##{__LINE__}]" do
-    (Otaky.work(&(proc { |arg1, arg2| %w{a b}.map{|x| puts x } }))).
-      should.be having_expected_proc_attrs(expected_file, __LINE__.pred, expected_code)
-  end
+    should "handle fanciful initializing with lambda do ... end [##{__LINE__}]" do
+      (
+        Otaky.work(&(lambda do |arg1, arg2|
+          %w{a b}.map{|x| puts x }
+        end))
+      ).should.be having_expected_proc_attrs(expected_file, __LINE__ - 3, expected_code)
+    end
 
-  should "handle fanciful initializing with proc do ... end [##{__LINE__}]" do
-    (
-      Otaky.work(&(proc do |arg1, arg2|
-        %w{a b}.map{|x| puts x }
-      end))
-    ).should.be having_expected_proc_attrs(expected_file, __LINE__ - 3, expected_code)
-  end
+    should "handle fanciful initializing with proc { ... } [##{__LINE__}]" do
+      (Otaky.work(&(proc { |arg1, arg2| %w{a b}.map{|x| puts x } }))).
+        should.be having_expected_proc_attrs(expected_file, __LINE__.pred, expected_code)
+    end
 
-  should "handle fanciful initializing with Proc.new { ... } [##{__LINE__}]" do
-    (Otaky.work(&(Proc.new { |arg1, arg2| %w{a b}.map{|x| puts x } }))).
-      should.be having_expected_proc_attrs(expected_file, __LINE__.pred, expected_code)
-  end
+    should "handle fanciful initializing with proc do ... end [##{__LINE__}]" do
+      (
+        Otaky.work(&(proc do |arg1, arg2|
+          %w{a b}.map{|x| puts x }
+        end))
+      ).should.be having_expected_proc_attrs(expected_file, __LINE__ - 3, expected_code)
+    end
 
-  should "handle fanciful initializing with Proc.new do ... end [##{__LINE__}]" do
-    (
-      Otaky.work(&(Proc.new do |arg1, arg2|
-        %w{a b}.map{|x| puts x }
-      end))
-    ).should.be having_expected_proc_attrs(expected_file, __LINE__ - 3, expected_code)
+    should "handle fanciful initializing with Proc.new { ... } [##{__LINE__}]" do
+      (Otaky.work(&(Proc.new { |arg1, arg2| %w{a b}.map{|x| puts x } }))).
+        should.be having_expected_proc_attrs(expected_file, __LINE__.pred, expected_code)
+    end
+
+    should "handle fanciful initializing with Proc.new do ... end [##{__LINE__}]" do
+      (
+        Otaky.work(&(Proc.new do |arg1, arg2|
+          %w{a b}.map{|x| puts x }
+        end))
+      ).should.be having_expected_proc_attrs(expected_file, __LINE__ - 3, expected_code)
+    end
+
   end
 
 end
--- spec/extending/new_matcher_w_single_arity_spec.rb
+++ spec/extending/new_matcher_w_single_arity_spec.rb
@@ -2,75 +2,79 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 
 describe 'Subclassing w single arity' do
 
-  extend SerializableProc::Spec::Helpers
   expected_file = File.expand_path(__FILE__)
   expected_code = "lambda { |lvar_arg| [\"a\", \"b\"].map { |lvar_x| puts(lvar_x) } }"
 
-  behaves_like 'has support for parsing Otaky.work (wo args)'
+  describe '>> wo args' do
 
-  should "handle block using do ... end [##{__LINE__}]" do
-    (
-      Otaky.work do |arg|
-        %w{a b}.map{|x| puts x }
-      end
-    ).should.be having_expected_proc_attrs(expected_file, __LINE__ - 3, expected_code)
-  end
+    extend SerializableProc::Spec::Helpers
+    behaves_like 'has support for parsing Otaky.work (wo args)'
 
-  should "handle block using do ... end [##{__LINE__}]" do
-    (Otaky.work do |arg| %w{a b}.map{|x| puts x } end).
-      should.be having_expected_proc_attrs(expected_file, __LINE__.pred, expected_code)
-  end
+    should "handle block using do ... end [##{__LINE__}]" do
+      (
+        Otaky.work do |arg|
+          %w{a b}.map{|x| puts x }
+        end
+      ).should.be having_expected_proc_attrs(expected_file, __LINE__ - 3, expected_code)
+    end
 
-  should "handle block using { ... } [##{__LINE__}]" do
-    (
-      Otaky.work { |arg|
-        %w{a b}.map{|x| puts x }
-      }
-    ).should.be having_expected_proc_attrs(expected_file, __LINE__ - 3, expected_code)
-  end
+    should "handle block using do ... end [##{__LINE__}]" do
+      (Otaky.work do |arg| %w{a b}.map{|x| puts x } end).
+        should.be having_expected_proc_attrs(expected_file, __LINE__.pred, expected_code)
+    end
 
-  should "handle block using { ... } [##{__LINE__}]" do
-    (Otaky.work { |arg| %w{a b}.map{|x| puts x } }).
-      should.be having_expected_proc_attrs(expected_file, __LINE__.pred, expected_code)
-  end
+    should "handle block using { ... } [##{__LINE__}]" do
+      (
+        Otaky.work { |arg|
+          %w{a b}.map{|x| puts x }
+        }
+      ).should.be having_expected_proc_attrs(expected_file, __LINE__ - 3, expected_code)
+    end
 
-  should "handle fanciful initializing with lambda { ... } [##{__LINE__}]" do
-    (Otaky.work(&(lambda { |arg| %w{a b}.map{|x| puts x } }))).
-      should.be having_expected_proc_attrs(expected_file, __LINE__.pred, expected_code)
-  end
+    should "handle block using { ... } [##{__LINE__}]" do
+      (Otaky.work { |arg| %w{a b}.map{|x| puts x } }).
+        should.be having_expected_proc_attrs(expected_file, __LINE__.pred, expected_code)
+    end
 
-  should "handle fanciful initializing with lambda do ... end [##{__LINE__}]" do
-    (
-      Otaky.work(&(lambda do |arg|
-        %w{a b}.map{|x| puts x }
-      end))
-    ).should.be having_expected_proc_attrs(expected_file, __LINE__ - 3, expected_code)
-  end
+    should "handle fanciful initializing with lambda { ... } [##{__LINE__}]" do
+      (Otaky.work(&(lambda { |arg| %w{a b}.map{|x| puts x } }))).
+        should.be having_expected_proc_attrs(expected_file, __LINE__.pred, expected_code)
+    end
 
-  should "handle fanciful initializing with proc { ... } [##{__LINE__}]" do
-    (Otaky.work(&(proc { |arg| %w{a b}.map{|x| puts x } }))).
-      should.be having_expected_proc_attrs(expected_file, __LINE__.pred, expected_code)
-  end
+    should "handle fanciful initializing with lambda do ... end [##{__LINE__}]" do
+      (
+        Otaky.work(&(lambda do |arg|
+          %w{a b}.map{|x| puts x }
+        end))
+      ).should.be having_expected_proc_attrs(expected_file, __LINE__ - 3, expected_code)
+    end
 
-  should "handle fanciful initializing with proc do ... end [##{__LINE__}]" do
-    (
-      Otaky.work(&(proc do |arg|
-        %w{a b}.map{|x| puts x }
-      end))
-    ).should.be having_expected_proc_attrs(expected_file, __LINE__ - 3, expected_code)
-  end
+    should "handle fanciful initializing with proc { ... } [##{__LINE__}]" do
+      (Otaky.work(&(proc { |arg| %w{a b}.map{|x| puts x } }))).
+        should.be having_expected_proc_attrs(expected_file, __LINE__.pred, expected_code)
+    end
 
-  should "handle fanciful initializing with Proc.new { ... } [##{__LINE__}]" do
-    (Otaky.work(&(Proc.new { |arg| %w{a b}.map{|x| puts x } }))).
-      should.be having_expected_proc_attrs(expected_file, __LINE__.pred, expected_code)
-  end
+    should "handle fanciful initializing with proc do ... end [##{__LINE__}]" do
+      (
+        Otaky.work(&(proc do |arg|
+          %w{a b}.map{|x| puts x }
+        end))
+      ).should.be having_expected_proc_attrs(expected_file, __LINE__ - 3, expected_code)
+    end
 
-  should "handle fanciful initializing with Proc.new do ... end [##{__LINE__}]" do
-    (
-      Otaky.work(&(Proc.new do |arg|
-        %w{a b}.map{|x| puts x }
-      end))
-    ).should.be having_expected_proc_attrs(expected_file, __LINE__ - 3, expected_code)
+    should "handle fanciful initializing with Proc.new { ... } [##{__LINE__}]" do
+      (Otaky.work(&(Proc.new { |arg| %w{a b}.map{|x| puts x } }))).
+        should.be having_expected_proc_attrs(expected_file, __LINE__.pred, expected_code)
+    end
+
+    should "handle fanciful initializing with Proc.new do ... end [##{__LINE__}]" do
+      (
+        Otaky.work(&(Proc.new do |arg|
+          %w{a b}.map{|x| puts x }
+        end))
+      ).should.be having_expected_proc_attrs(expected_file, __LINE__ - 3, expected_code)
+    end
+
   end
 
 end
